7eaa46ce9ccd7410274996591d854da0189fc472
PranavPenmatcha-os <pvp6360@nyu.edu>
2025-02-24 17:59:28 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -19,7 +19,6 @@ module tt_um_adder (
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
-  assign uio_in = 0;
   assign uio_oe  = uo_out;
 
   // List all unused inputs to prevent warnings
